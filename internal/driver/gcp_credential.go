aa06eb87d0c122955c15f8dfe45f92ff43d3e7bf
murasame29 <oogiriminister@gmail.com>
2024-05-04 07:32:05 +0000

    fix: cred

--- internal/driver/gcp_credential.go
+++ internal/driver/gcp_credential.go
@@ -1,10 +1,20 @@
 package driver
 
 import (
+	"log"
+	"os"
+
 	"github.com/murasame29/hackathon-util/cmd/config"
 	"google.golang.org/api/option"
 )
 
 func NewGCPCredential() option.ClientOption {
+	if config.Config.Google.Credentials == "" {
+		return nil
+	}
+
+	if _, err := os.Open(config.Config.Google.Credentials); err != nil {
+		log.Println(err)
+	}
 	return option.WithCredentialsFile(config.Config.Google.Credentials)
 }
